44328747d5cad4fbef872e80dfc16c8ad174c8bc
Dang Nguyen <117255808+g-n-a-d@users.noreply.github.com>
2024-01-08 18:44:00 +0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -52,6 +52,8 @@ class CondenserForPretraining(nn.Module):
         self.train_args = train_args
         self.data_args = data_args
 
+        self._keys_to_ignore_on_save = None
+
     def forward(self, model_input, labels):
         attention_mask = self.lm.get_extended_attention_mask(
             model_input['attention_mask'],
